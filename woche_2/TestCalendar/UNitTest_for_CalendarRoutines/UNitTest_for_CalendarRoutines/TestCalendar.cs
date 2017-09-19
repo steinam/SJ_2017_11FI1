d5d7fa32c1d5a11983b04b024cee13ef8137a784
steinam <steinam@gmail.com>
2017-09-19 10:04:38 +0200

    Implementierung von NUnit-Grundfunktionalitäten (Anlage, TestFixture, Test, Exception)

---
+++ woche_2/TestCalendar/UNitTest_for_CalendarRoutines/UNitTest_for_CalendarRoutines/TestCalendar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using KalendarRoutines;
+
+
+
+
+namespace UNitTest_for_CalendarRoutines
+{
+
+    [TestFixture] //TestKlasse
+    public class TestCalendar
+    {
+
+
+
+
+        [Test]
+        public void Test_DayOfWeek_Returns_Correct_Weekday()
+        {
+
+            //arrange
+            //Variablen definieren
+            //DB-Connectipon erzeugen
+
+            //act
+
+
+            //assert
+            //Überprüfung von erwartetem und tatsächlichem Ergebnis
+            Assert.AreEqual(20, Calendar.d_year(20, 1, 2017));
+            Assert.AreEqual(61, Calendar.d_year(1, 3, 2016));
+            Assert.AreEqual(365, Calendar.d_year(-1, 12, 2017));
+
+        }
+
+        
+        [Test]
+        public void Test_If_Day_Year_Throws_Exception_When_Negative_Day()
+        {
+
+            Assert.Throws<System.FormatException>(() => Calendar.d_year(-3, 12, 2017));
+        }
+
+
+    }
+}
